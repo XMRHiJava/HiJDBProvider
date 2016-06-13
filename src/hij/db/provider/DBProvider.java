a0593fc91bb9f9b3b10159336203adf03e80a27a
XuminRong <xumingxsh21@126.com>
2016-06-13 16:43:46 +0800

    Signed-off-by: XuminRong <xumingxsh21@126.com> 添加注释

--- src/hij/db/provider/DBProvider.java
+++ src/hij/db/provider/DBProvider.java
@@ -10,6 +10,14 @@ import hij.util.generic.IActionP1;
 import hij.util.generic.IFuncP1;
 
 public final class DBProvider {
+	/**
+	 * 返回队列(要求队列中的属性与查询字段名称相同)
+	 * @param cls 队列中存储的数据类型
+	 * @param param SQL参数(SQLParams为自定义类型)
+	 * @param fun SQL中存在参数时,使用该函数设置参数(使用自定义的含名称参数)
+	 * @param args SQL中包含格式化字符串时,输入的相关数据
+	 * @return 队列
+	 */
 	public static <T> List<T> executeQuery(Class<T> cls, SQLParams param, IFuncP1<String, Object> fun, Object...args) {
 		if (cls == null || param == null) {
 			return null;
@@ -35,6 +43,15 @@ public final class DBProvider {
 	}
 	
 
+	/**
+	 * 
+	 * @param cls
+	 * @param param
+	 * @param fun
+	 * @param args
+	 * @return
+	 * @throws SQLException
+	 */
 	public static <T> T executeScalar(Class<T> cls, SQLParams param, IFuncP1<String, Object> fun, Object...args) throws SQLException {
 		if (cls == null || param == null) {
 			return null;
